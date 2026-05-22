2b27cbd1a1eda9fbc56fd95d2a0697c916038774
sabnira <sabnirajeemi@gmail.com>
2026-05-23 01:01:29 +0600

    admin chart

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,21 +1,195 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../providers/AuthProvider";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { FaBook, FaCar, FaDollarSign, FaUsers } from "react-icons/fa";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  LabelList,
+  Label,
+  Tooltip,
+  ResponsiveContainer, PieChart, Pie, Cell, Legend
+} from 'recharts';
+
+
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink', 'black'];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#F43098'];
+const RADIAN = Math.PI / 180;
 
 
 const AdminHome = () => {
 
-    const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+
+  //use tanstack
+  const { data: stats = {} } = useQuery({
+    queryKey: ['admin-stats'],
+    queryFn: async () => {
+      const res = await axiosSecure.get('/admin-stats')
+      return res.data
+    }
+  })
+
+  const { data: chartData = [] } = useQuery({
+    queryKey: ['order-stats'],
+    queryFn: async () => {
+      const res = await axiosSecure.get('/order-stats')
+      return res.data
+    }
+  })
+
+  //custom shape for the bar chart
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { x, y, width, height, index } = props;
+
+    const color = colors[index % colors.length];
 
     return (
-        <div>
-            <h2 className="text3xl">
-                <span>Hi, Welcome </span>
-                {
-                    user?.displayName ? user.displayName : 'Back'
-                }
-            </h2>
-        </div>
+      <path
+        strokeWidth={props.isActive ? 5 : 0}
+        d={getPath(Number(x), Number(y), Number(width), Number(height))}
+        stroke={color}
+        fill={color}
+        style={{
+          transition: 'stroke-width 0.3s ease-out',
+        }}
+      />
     );
+  };
+
+  const CustomColorLabel = (props) => {
+    const fill = colors[(props.index ?? 0) % colors.length];
+    return <Label {...props} fill={fill} />;
+  };
+
+
+  //custom shape for pie chart
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const pieChartData = chartData.map(data => {
+    return { name: data.category, value: data.revenue }
+  })
+
+  return (
+    <div>
+
+      <h2 className="text-3xl">
+        <span>Hi, Welcome </span>
+        {
+          user?.displayName ? user.displayName : 'Back'
+        }
+      </h2>
+
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats?.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl"></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats?.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl"></FaBook>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats?.menuItems}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaCar className="text-3xl"></FaCar>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats?.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+
+      <div className="flex">
+
+        <div className="w-1/2">
+          <BarChart
+            style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+            responsive
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 0,
+              left: 0,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip cursor={{ fillOpacity: 0.5 }} />
+            <XAxis dataKey="category" />
+            <YAxis width="auto" />
+            <Bar dataKey="quantity" fill="#8884d8" shape={TriangleBar} activeBar>
+              <LabelList content={CustomColorLabel} position="top" />
+            </Bar>
+
+          </BarChart>
+        </div>
+
+        <div className="w-1/2">
+          <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+              <Pie
+                data={pieChartData}
+                labelLine={false}
+                label={renderCustomizedLabel}
+                dataKey="value"
+              >
+                {pieChartData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+
+      </div>
+
+    </div>
+  );
 };
 
 export default AdminHome;
